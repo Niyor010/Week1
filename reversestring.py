a7750bdfef336f38a8ac7bea986e96e7a419665f
Niyor010 <niyorbarman2@gmail.com>
2025-02-03 23:33:49 +0530

    Add files via upload

---
+++ reversestring.py
@@ -0,0 +1,10 @@
+# Function to reverse a string
+def reverse_string(input_string):
+    return input_string[::-1]
+
+# Example usage
+original_string = "Hello, world!"
+reversed_string = reverse_string(original_string)
+
+print("Original string:", original_string)
+print("Reversed string:", reversed_string)
