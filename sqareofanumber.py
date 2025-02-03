b0e421de3123517bfcf3f5c50413dfbd3580bdcd
Niyor010 <niyorbarman2@gmail.com>
2025-02-03 23:32:57 +0530

    Add files via upload

---
+++ sqareofanumber.py
@@ -0,0 +1,11 @@
+# Example Python program to find the square of a number
+
+# Function to find the square
+def find_square(num):
+    return num ** 2
+
+# Example usage
+number = 6
+square = find_square(number)
+
+print("The square of {} is: {}".format(number, square))
