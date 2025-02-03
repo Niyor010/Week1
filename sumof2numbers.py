fd36a944f6c8ed669cb9a7d7518dbeca578923ed
Niyor010 <niyorbarman2@gmail.com>
2025-02-03 23:28:04 +0530

    Add files via upload

---
+++ sumof2numbers.py
@@ -0,0 +1,12 @@
+# Example Python program to add two numbers
+
+# Function to add two numbers
+def add_numbers(a, b):
+    return a + b
+
+# Example usage
+num1 = 5
+num2 = 3
+sum = add_numbers(num1, num2)
+
+print("The sum of {} and {} is: {}".format(num1, num2, sum))
